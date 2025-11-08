fab74a2c2fb4ba3016a20367e227fe543dbf70c7
ferdaous-mab <mabroukiferdaous@gmail.com>
2025-11-08 19:06:42 +0100

    Supp les dern modif

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ from controllers.plagiat_controller import plagiat_bp
 app = Flask(__name__)
 CORS(app)
 
-# Enregistre ton blueprint
 app.register_blueprint(plagiat_bp)
 
-# Routes principales
 @app.route('/')
 def home():
     return render_template('login.html')
@@ -17,10 +15,10 @@ def home():
 def login():
     return render_template('login.html')
 
+# Nouvelle route pour index
 @app.route('/index')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    # Pour tests locaux seulement
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True)
